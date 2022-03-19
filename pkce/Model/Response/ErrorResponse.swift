1366306ae784068a650bf2b3b18aa08c3403f67f
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-03-19 02:39:32 -0500

    feat: add api client

---
+++ pkce/Model/Response/ErrorResponse.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorResponse.swift
+//  pkce
+//
+//  Created by Ricardo Bravo on 19/03/22.
+//
+
+import Foundation
+
+struct ErrorResponse: Codable {
+    let id: String
+    let message: String
+}
+
+extension ErrorResponse: LocalizedError {
+    var errorDescription: String? {
+        return error
+    }
+}
